d9fa7b399c1381aa8b27977982dc668a310f3137
Yohei Takano <y.h.takano@gmail.com>
2024-03-08 17:30:59 +0000

    Make an adjoint input files directory.

---
+++ global_ocn3deg_bgcv0/input_ad/input_ad_kpp_atmco2pv0/pickup/rename_pickup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp pickup.0002160000.data pickup.0000000001.data
+cp pickup.0002160000.meta pickup.0000000001.meta
+cp pickup_cd.0002160000.data pickup_cd.0000000001.data
+cp pickup_cd.0002160000.meta pickup_cd.0000000001.meta
+cp pickup_dic.0002160000.data pickup_dic.0000000001.data
+cp pickup_dic.0002160000.meta pickup_dic.0000000001.meta
+cp pickup_ptracers.0002160000.data pickup_ptracers.0000000001.data
+cp pickup_ptracers.0002160000.meta pickup_ptracers.0000000001.meta
+
