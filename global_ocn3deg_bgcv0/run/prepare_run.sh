d741805062916ce2233095a0571d16f7ed0ecc91
Yohei Takano <47384280+ytakano3@users.noreply.github.com>
2024-03-27 10:23:32 +0000

    Minor update prepare_run.sh

--- global_ocn3deg_bgcv0/run/prepare_run.sh
+++ global_ocn3deg_bgcv0/run/prepare_run.sh
@@ -2,8 +2,9 @@
 
 ln -s ../build/mitgcmuv .
 ln -s ../input/input_kpp_atmco2pv0/* .
-ln -s ../input/input_kpp_atmco2pv0/ocean_bgc/*.bin .
-ln -s ../input/input_kpp_atmco2pv0/ocean_phys/*.bin .
+#ln -s ../input/input_kpp_atmco2pv0/ocean_bgc/*.bin .
+#ln -s ../input/input_kpp_atmco2pv0/ocean_phys/*.bin .
 ln -s ../input/input_kpp_atmco2pv0/pickup/* .
 
 # The idea would be to run this before submitting a slurm script
+# No need to copy binary files from ocean_XXX, set to directly read these files
