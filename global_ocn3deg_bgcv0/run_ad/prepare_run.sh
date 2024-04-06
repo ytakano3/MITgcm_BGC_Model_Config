392d8d40b81946f241a692f1e43fbf785d649d79
Yohei Takano <47384280+ytakano3@users.noreply.github.com>
2024-04-06 01:47:44 +0100

    Minor update prepare_run.sh

--- global_ocn3deg_bgcv0/run_ad/prepare_run.sh
+++ global_ocn3deg_bgcv0/run_ad/prepare_run.sh
@@ -5,7 +5,7 @@ ln -s ../input_ad/input_kpp_atmco2pv0/* .
 #ln -s ../input_ad/input_kpp_atmco2pv0/ocean_bgc/*.bin .
 #ln -s ../input_ad/input_kpp_atmco2pv0/ocean_phys/*.bin .
 ln -s ../input_ad/input_kpp_atmco2pv0/pickup/* .
-ln -s ../input_ad/input_kpp_atmco2pv0/adjoint/* .
+ln -s ../input_ad/input_kpp_atmco2pv0/masks/* .
 
 # The idea would be to run this before submitting a slurm script
 # No need to copy binary files from ocean_XXX, set to directly read these files
